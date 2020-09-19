41c2fc9e5c79d62de20128655a8f16056ff64466
Arnaud Duhé <anro010@hotmail.com>
2020-09-19 11:34:54 +0100

    main part of logic done

--- frontend/src/components/Skills.js
+++ frontend/src/components/Skills.js
@@ -3,16 +3,25 @@ import skills from '../data/skills.json';
 
 function Skills() {
 
-    const tech = skills[0].technology
+    const tech = skills[0].technology[0] 
+    const type = skills[0].type[0] 
+
 
     return (
         <div>
             <h1>Welcome to my Skills Page</h1>
             <p>Here I rate myself how comfortable I feel with a technology</p>
             <div className="container-skills">
+                <div className="container-skills-technology">
+                    <ul>
+                        {Object.keys(tech).map(skill => 
+                            <li>{skill +": "+tech[skill]}</li>)}
+                    </ul>
+                </div>
                 <div className="container-skills-type">
                     <ul>
-                        <li>{tech.map(skill => `${Object.keys(skill)}: ${Object.values(skill)}`)}</li>
+                        {Object.keys(type).map(skill => 
+                            <li>{skill +": "+type[skill]}</li>)}
                     </ul>
                 </div>
             </div>
